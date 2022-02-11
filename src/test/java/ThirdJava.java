b4dc79d58a3d5f8a7a90248f10e09eccdd9d5c73
furkanmc <frknuk@gmail.com>
2022-02-11 19:34:34 +0000

    my new branch

--- src/test/java/ThirdJava.java
+++ src/test/java/ThirdJava.java
@@ -2,5 +2,6 @@ public class ThirdJava {
     public static void main(String[] args) {
         System.out.println("new branch");
         System.out.println("2. new branch");
+        System.out.println("3. change");
     }
 }
